8d5c4b4ef8d47e865164f3ba45b2dc6e2cbff078
olia1606 <oliapekunova@gmail.com>
2024-12-05 23:24:57 +0300

    Добавила перевод на русский

--- users/models.py
+++ users/models.py
@@ -7,5 +7,9 @@ class UserProfile(models.Model):
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     address = models.CharField(max_length=255, blank=True, null=True)
 
+    class Meta:
+        verbose_name_plural = "Профили пользователей"
+        verbose_name = "Профиль пользователя"
+
     def __str__(self):
         return self.user.username
